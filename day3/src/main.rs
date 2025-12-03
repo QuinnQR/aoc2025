96039596e811b3b411dabf9884b7fbf7dbfa6b7d
QQR <sam.ralph8@gmail.com>
2025-12-03 08:41:41 +0000

    Solve day 3 part 2, refactor day 3 part 1

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -13,34 +13,42 @@ fn main() {
     println!("\tDay 3\nPart 1: {}\nPart 2: {}", part1, part2);
 }
 fn calculate_answers(line_iterator: Box<dyn Iterator<Item = String>>) -> (Int, Int) {
-    let mut part1 = 0;
-    let part2 = 0;
+    let mut part1: Int = 0;
+    let mut part2: Int = 0;
+
     for line in line_iterator {
-        let mut biggest_so_far = 0;
-        let mut current_second = 0;
-        for char in line.chars() {
-            let num = char as Int;
-            if num > biggest_so_far {
-                current_second = -biggest_so_far;
-                biggest_so_far = num;
-            } else if num > current_second {
-                current_second = num;
+        let mut part_two_digits: [Int; 12] = [0; 12];
+        let mut part_one_digits: [Int; 2] = [0; 2];
+        // Stores the digit of the best
+        for (i, digit) in line.chars().enumerate() {
+            let starting_index = 11 - (line.len() - i - 1).min(11);
+            let val = digit as Int - '0' as Int;
+            for index in starting_index..12 {
+                if val > part_two_digits[index] {
+                    part_two_digits[index] = val;
+                    part_two_digits[(index + 1)..12].fill(0);
+                    break;
+                }
+            }
+            if val > part_one_digits[0] && i != line.len() - 1 {
+                part_one_digits[0] = val;
+                part_one_digits[1] = 0;
+            } else if val > part_one_digits[1] {
+                part_one_digits[1] = val;
             }
         }
-        part1 += if current_second < 0 {
-            10 * (-current_second - ('0' as Int)) + biggest_so_far - ('0' as Int)
-        } else {
-            10 * (biggest_so_far - ('0' as Int)) + current_second - ('0' as Int)
-        }
+        part1 += part_one_digits.into_iter().reduce(|lhs, rhs| lhs * 10 + rhs).unwrap();
+        part2 += part_two_digits.into_iter().reduce(|lhs, rhs| lhs * 10 + rhs).unwrap();
     }
     (part1, part2)
 }
+
 fn read_input<P>(filename: P) -> Result<Box<dyn Iterator<Item = String>>, Box<dyn Error>>
 where
     P: AsRef<Path>,
 {
     Ok(Box::new(
-        get_lines_from_file(filename)?.map_while(|x| x.ok().filter(|x| x.len() > 1)),
+        get_lines_from_file(filename)?.map_while(|x| x.ok().filter(|x| x.len() > 11)),
     ))
 }
 
@@ -64,5 +72,11 @@ mod test {
         let test_iterator = read_input("test").unwrap();
         let (part1, _part2) = calculate_answers(test_iterator);
         assert_eq!(part1, 357);
+    }
+    #[test]
+    fn test_part2() {
+        let test_iterator = read_input("test").unwrap();
+        let (_part1, part2) = calculate_answers(test_iterator);
+        assert_eq!(part2, 3121910778619);
     }
 }
